197f01e77aee505e2538c8d8a1bd8c5f46c30fc2
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2023-01-17 16:42:26 +0100

    bloccato su technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -26,5 +27,10 @@ class Project extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\support\Str;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $technologies = ['html', 'css', 'javascript', 'php', 'blade'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, "-");
+            $newTechnology->save();
+        }
+    }
+}
